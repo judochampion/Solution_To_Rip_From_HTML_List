42b3adec41b0a4d8fd29a520af214d4fdf673af2
Joachim Ally <joachimally@gmail.com>
2022-03-09 00:41:36 +0100

    First version with some rudimental development

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using List_Ripper;
+
+Console.WriteLine("Hello, World!");
+
+if ((new Class1().Hello_World()))
+{
+    Console.WriteLine("Success");
+}
+
+Console.Read();
---
+++ List_Ripper/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace List_Ripper
+{
+    public class Class1
+    {
+        public bool Hello_World()
+        {
+            var test = Directory.GetDirectories(@"..\..\Project_1000_Klassiekers");
+            
+            return true;
+        }
+    }
+}
